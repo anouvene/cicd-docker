6f676e2f806be7c1740703673413ccbdcac31b8e
Tuan <nouvenbe.antoine@gmail.com>
2025-04-10 04:13:41 +0200

    Fix e2e test login & logout

--- frontend/cypress/support/types.d.ts
+++ frontend/cypress/support/types.d.ts
@@ -3,12 +3,13 @@
 declare global {
     namespace Cypress {
         interface Chainable {
+            dataCy(value: string): Chainable<JQuery<HTMLElement>>
             login(email: string, password: string): Chainable<void>
             //   drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
             //   dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
             //   visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
 
-            dataCy(value: string): Chainable<JQuery<HTMLElement>>
+            
         }
     }
 }
